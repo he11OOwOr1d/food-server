decfe958d70a25fc1fb17c6935d2079b4c92f05a
he11OOwOr1d <151227308+he11OOwOr1d@users.noreply.github.com>
2024-09-19 20:21:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const fetch = require('cross-fetch');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const defaultHeaders = {
-  'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'
-};
-
+app.use(cors()); 
 app.get('/api/restaurants', async (req, res) => {
   try {
     const response = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.3008241&lng=73.1733127&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING", 
-      { headers: defaultHeaders }
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+      }
     );
 
     if (response.ok) {
@@ -25,15 +31,21 @@ app.get('/api/restaurants', async (req, res) => {
 });
 
 app.get('/api/menu/:resId', async (req, res) => {
-  const { resId } = req.params; 
+  const { resId } = req.params;
   const menuUrl = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.9905081&lng=76.9873477&restaurantId=${resId}`;
 
   try {
-    const response = await fetch(menuUrl, { headers: defaultHeaders });
+    const response = await fetch(menuUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    });
 
     if (response.ok) {
       const data = await response.json();
-      res.status(200).json(data); 
+      res.status(200).json(data);
     } else {
       res.status(response.status).send('Error fetching menu data');
     }
@@ -42,6 +54,7 @@ app.get('/api/menu/:resId', async (req, res) => {
   }
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
